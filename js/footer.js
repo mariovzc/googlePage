de2030f6b9ed67c073cc83e9bf4f03c94195cb5d
¨Mario <mario.vizcainos@gmail.com>
2017-01-31 23:07:55 -0500

    getting ready to create components

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+/*
+<footer>
+    <div class="left">
+        <ul>
+            <li><a href="javascript:void(0)">Publicidad</a></li>
+            <li><a href="javascript:void(0)">Negocios</a></li>
+            <li><a href="javascript:void(0)">Acerca de</a></li>
+        </ul>
+    </div>
+    <div class="right">
+        <ul>
+            <li><a href="javascript:void(0)">Privacidad</a></li>
+            <li><a href="javascript:void(0)">Condiciones</a></li>
+            <li><a href="javascript:void(0)">Preferencias</a></li>
+            <li><a href="javascript:void(0)">Usar Google.com</a></li>
+        </ul>
+    </div>
+</footer>
+ */
