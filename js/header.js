b02fc05f9af551e017002232b26a1c98d0575cb7
¨Mario <mario.vizcainos@gmail.com>
2017-01-31 23:28:03 -0500

    refactor of the header function

--- js/header.js
+++ js/header.js
@@ -15,21 +15,13 @@ function header() {
     var ul = Ul();
     var li;
 
-    li = list();
-    li.appendChild(Hyperlink("Gmail", ""));
-    ul.appendChild(li);
-
-    li = list();
-    li.appendChild(Hyperlink("Images", ""));
-    ul.appendChild(li);
-
-    li = list();
-    li.appendChild(Hyperlink("Menu", ""));
-    ul.appendChild(li);
-
-    li = list();
-    li.appendChild(Hyperlink("Bell", ""));
-    ul.appendChild(li);
+    var words = ["Gmail", "Images", "Menu", "Bell"];
+    
+    for(i = 0; i < words.length; i++){
+        li = list();
+        li.appendChild(Hyperlink(words[i], ""));
+        ul.appendChild(li);
+    }
 
     li = list();
     li.appendChild(Hyperlink("M", "user"));
